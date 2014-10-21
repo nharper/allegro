8b3d8ecb4e71451a2df60266b7973d8b06e05a79
Nick Harper <nharper@nharper.org>
2014-10-20 22:34:03 -0700

    Initial rails project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Allegro::Application.config.secret_key_base = '23694a916c6dfff1864f76602ad0d571f1ef2dc24833e66c5da66e4b4dff45241ee3815fa71749782a58dbc5b2f801080777bc102f310b3ccd4822f00dcd57c8'
