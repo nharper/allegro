89e5290de1386b02ba1f739b39d514b7d3647653
Nick Harper <nharper@nharper.org>
2015-05-26 21:10:56 -0400

    Refactor/rewrite js and redo local storage

--- app/assets/javascripts/attendance.js
+++ app/assets/javascripts/attendance.js
@@ -1,73 +1,101 @@
-function updateTile(node, state) {
-  var select = node.querySelector('select');
-	switch (state) {
-		case 'present':
-			node.classList.remove('present-false');
-			node.classList.remove('present-unknown');
-			node.classList.add('present-true');
-      select.value = 'present';
-			break;
-		case 'absent':
-			node.classList.add('present-false');
-			node.classList.remove('present-unknown');
-			node.classList.remove('present-true');
-      select.value = 'absent';
-			break;
-		default:
-			node.classList.remove('present-false');
-			node.classList.add('present-unknown');
-			node.classList.remove('present-true');
-      select.value = '';
-			break;
-	}
-}
-
-/*
-function saveRoster() {
-  var stored_roster = {data: {}, version: 1};
-  roster.forEach(function(performer) {
-    stored_roster.data[performer.number] = performer;
-  });
-	window.localStorage.attendance = JSON.stringify(stored_roster);
-	var record = "";
-}
-
-function clearRoster() {
-	window.localStorage.attendance = '';
-  window.location.reload();
-}
-*/
-
-function toggleTile(e) {
-  if (e.target.nodeName == 'SELECT') {
-    return;
+function AttendanceGrid(root, storage_key) {
+  /**
+   * Updates the display of a tile |node| to state |state|. This updates the
+   * color of the div and the value in the select.
+   */
+  this.updateTile = function(node, state) {
+    var select = node.querySelector('select');
+    switch (state) {
+      case 'present':
+        node.classList.remove('present-false');
+        node.classList.remove('present-unknown');
+        node.classList.add('present-true');
+        select.value = 'present';
+        break;
+      case 'absent':
+        node.classList.add('present-false');
+        node.classList.remove('present-unknown');
+        node.classList.remove('present-true');
+        select.value = 'absent';
+        break;
+      default:
+        node.classList.remove('present-false');
+        node.classList.add('present-unknown');
+        node.classList.remove('present-true');
+        select.value = '';
+        break;
+    }
+    this.saveState(select.name, select.value);
   }
-  var value = this.querySelector('select').value;
-  if (value == '') {
-    updateTile(this, 'present');
-  } else if (value == 'present') {
-    updateTile(this, 'absent');
-  } else {
-    updateTile(this, '');
-  }
-}
 
-function updateTileFromSelect() {
-  updateTile(this.parentNode, this.value);
+  this.saveState = function(name, value) {
+    this.roster_[name] = value;
+    window.localStorage.setItem(this.storage_key_, JSON.stringify({data: this.roster_, version: 1}));
+  }
+
+  this.restoreState = function() {
+    var stored_state = {};
+    try {
+      stored_state = JSON.parse(window.localStorage.getItem(this.storage_key_));
+    } catch (e) {
+      console.log(e);
+    }
+    if (!stored_state || !stored_state.data) {
+      stored_state = {data: {}, version: 1};
+    }
+    if (stored_state.version != 1) {
+      stored_state.data = {};
+    }
+    return stored_state.data;
+  }
+
+  this.resetState = function() {
+    window.localStorage.removeItem(this.storage_key_);
+    this.roster_ = {};
+    var tiles =  this.container_.querySelectorAll('.tile');
+    for (var i = 0; i < tiles.length; i++) {
+      this.updateTile(tiles[i], '');
+    }
+  }
+
+  var toggleTile = function(e) {
+    if (e.target.nodeName == 'SELECT') {
+      return;
+    }
+    var value = e.target.querySelector('select').value;
+    if (value == '') {
+      this.updateTile(e.target, 'present');
+    } else if (value == 'present') {
+      this.updateTile(e.target, 'absent');
+    } else {
+      this.updateTile(e.target, '');
+    }
+  };
+  var updateTileFromSelect = function(e) {
+    this.updateTile(e.target.parentNode, e.target.value);
+  };
+  this.container_ = root;
+  this.storage_key_ = storage_key;
+  this.roster_ = {};
+
+  var restore_state = this.restoreState();
+  var tiles =  this.container_.querySelectorAll('.tile');
+  for (var i = 0; i < tiles.length; i++) {
+    tiles[i].addEventListener('click', toggleTile.bind(this));
+    var select = tiles[i].querySelector('select');
+    select.addEventListener('change', updateTileFromSelect.bind(this));
+
+    if (restore_state[select.name]) {
+      this.updateTile(tiles[i], restore_state[select.name]);
+    } else {
+      this.updateTile(tiles[i], select.value);
+    }
+  }
 }
 
 function documentLoaded() {
-  var container = document.getElementById("tiles");
-  var tiles = container.querySelectorAll(".tile");
-  for (var i = 0; i < tiles.length; i++) {
-    tiles[i].addEventListener('click', toggleTile);
-    tiles[i].querySelector('select').addEventListener('change', updateTileFromSelect);
-  }
-
-	// set up roster
-	roster = {};
-
-	// set up buttons
-	// document.getElementById("save").addEventListener('click', saveRoster);
-	// document.getElementById("reset").addEventListener('click', clearRoster);
+  attendanceGrid = new AttendanceGrid(document.getElementById("tiles"), 'attendance:T1L:5-25');
+  // set up buttons
+  var reset = document.getElementById('reset');
+  reset.addEventListener('click', attendanceGrid.resetState.bind(attendanceGrid));
 }
