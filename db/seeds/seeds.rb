bbcb6858e6d5e8953b758259e0acbf12f87d6a7b
Nick Harper <nharper@nharper.org>
2015-08-23 22:11:20 -0700

    Update db seeds

--- db/seeds/seeds.rb
+++ db/seeds/seeds.rb
@@ -6,49 +6,60 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-elton = Concert.create({
-    name: 'Test Concert',
-    start_date: Date.new(2015, 4, 6),
-    end_date: Date.new(3015, 6, 27)
+zone = ActiveSupport::TimeZone['Pacific Time (US & Canada)']
+
+holigays = Concert.create({
+    name: 'HoliGays',
+    start_date: Date.new(2015, 8, 24),
+    end_date: Date.new(2015, 12, 24)
     });
 
 Rehearsal.create([
-  { date: DateTime.new(2015, 4, 6, 19, 0, 0),
+  { date: zone.local_to_utc(DateTime.parse('2015-08-24 19:00')),
     attendance: :required,
-    concert: elton },
-  { date: DateTime.new(2015, 4, 13, 19, 0, 0),
+    concert: holigays },
+  { date: zone.local_to_utc(DateTime.parse('2015-08-31 19:00')),
     attendance: :required,
-    concert: elton },
-  { date: DateTime.new(2015, 4, 20, 19, 0, 0),
+    concert: holigays },
+  { date: zone.local_to_utc(DateTime.parse('2015-09-08 19:00')),
     attendance: :required,
-    concert: elton },
-  { date: DateTime.new(2015, 4, 27, 19, 0, 0),
+    concert: holigays },
+  { date: zone.local_to_utc(DateTime.parse('2015-09-14 19:00')),
     attendance: :required,
-    concert: elton },
-  { date: DateTime.new(2015, 5, 4, 19, 0, 0),
+    concert: holigays },
+  { date: zone.local_to_utc(DateTime.parse('2015-09-21 19:00')),
     attendance: :required,
-    concert: elton },
-  { date: DateTime.new(2015, 5, 11, 19, 0, 0),
+    concert: holigays },
+  { date: zone.local_to_utc(DateTime.parse('2015-09-28 19:00')),
     attendance: :required,
-    concert: elton },
-  { date: DateTime.new(2015, 5, 18, 19, 0, 0),
+    concert: holigays },
+  { date: zone.local_to_utc(DateTime.parse('2015-10-05 19:00')),
     attendance: :required,
-    concert: elton },
-  { date: DateTime.new(2015, 5, 25, 19, 0, 0),
+    concert: holigays },
+  { date: zone.local_to_utc(DateTime.parse('2015-10-12 19:00')),
     attendance: :required,
-    concert: elton },
-  { date: DateTime.new(2015, 6, 1, 19, 0, 0),
+    concert: holigays },
+  { date: zone.local_to_utc(DateTime.parse('2015-10-19 19:00')),
     attendance: :required,
-    concert: elton },
-  { date: DateTime.new(2015, 6, 8, 19, 0, 0),
+    concert: holigays },
+  { date: zone.local_to_utc(DateTime.parse('2015-10-26 19:00')),
     attendance: :required,
-    concert: elton },
-  { date: DateTime.new(2015, 6, 15, 19, 0, 0),
+    concert: holigays },
+  { date: zone.local_to_utc(DateTime.parse('2015-11-02 19:00')),
     attendance: :required,
-    concert: elton },
-  { date: DateTime.new(2015, 6, 22, 19, 0, 0),
+    concert: holigays },
+  { date: zone.local_to_utc(DateTime.parse('2015-11-09 19:00')),
     attendance: :required,
-    concert: elton }
+    concert: holigays },
+  { date: zone.local_to_utc(DateTime.parse('2015-11-16 19:00')),
+    attendance: :required,
+    concert: holigays },
+  { date: zone.local_to_utc(DateTime.parse('2015-11-23 19:00')),
+    attendance: :required,
+    concert: holigays },
+  { date: zone.local_to_utc(DateTime.parse('2015-11-30 19:00')),
+    attendance: :required,
+    concert: holigays },
   ]);
 
 [
@@ -69,5 +80,5 @@ Rehearsal.create([
     number: "125",
     section: "T1L"},
 ].each do |performer|
-  Performer.createWithRegistration(performer, elton)
+  Performer.createWithRegistration(performer, holigays)
 end
