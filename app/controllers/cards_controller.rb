c9ca2cb00a5043de80292bd9350b4fbc6ed42455
Nick Harper <nharper@nharper.org>
2015-09-20 14:07:10 -0700

    Redo card registration to allow registering multiple cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,23 +1,27 @@
 class CardsController < ApplicationController
-  def new
-    # Select registrations (with associated performer model) for performers
-    # that don't have cards.
-    @registrations = Registration.where(:concert => Concert.current).where.not(:performer_id => Card.where(:active => true).pluck(:performer_id)).includes(:performer).order('chorus_number')
-    # @performers = Performer.where.not(:id => Card.where(:active => true).pluck(:performer_id))
-  end
-
   def create
     card = Card.create(params[:card].permit(:card_id, :performer_id).merge(:active => true))
     if !card.valid?
       flash[:error] = 'Error creating new card'
     end
-    redirect_to new_card_path
+    redirect_to cards_path
   end
 
   def index
     respond_to do |format|
       format.html do
-        @cards = Card.all.includes(:performer).order('performers.name')
+        @registrations = Registration.where(:concert => Concert.current).order('chorus_number').includes(:performer)
+
+        # Group cards by performer
+        @cards = {}
+        Card.all.each do |card|
+          @cards[card.performer_id] = [] unless @cards[card.performer_id]
+          @cards[card.performer_id] << card
+        end
+
+        # Group registrationless performers:
+        @other_performers = Performer.where.not(:id => Registration.where(:concert => Concert.current).pluck(:performer_id)).order('name')
+
       end
       format.json do
         @cards = Card.select(:card_id, :performer_id).where(:active => true)
@@ -31,6 +35,6 @@ class CardsController < ApplicationController
     if !card.destroy
       flash[:error] = 'Error deleting card'
     end
-    redirect_to new_card_path
+    redirect_to cards_path
   end
 end
