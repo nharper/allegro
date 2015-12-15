09fde51a7f7ea7b36e1756d2451122fa725fbbb4
Nick Harper <nharper@nharper.org>
2015-12-14 21:16:01 -0800

    Rewrite /attendance/list with react (feature parity)

---
+++ app/assets/javascripts/components/attendance_list.js.jsx
@@ -0,0 +1,112 @@
+var AttendanceList = React.createClass({
+  'rehearsals': function() {
+    return this.props.rehearsals.map(function(rehearsal) {
+      var reh = rehearsal;
+      rehearsal.start_date = new Date(rehearsal.start_date);
+      rehearsal.end_date = new Date(rehearsal.end_date);
+      return rehearsal;
+    });
+  },
+
+  'render': function() {
+    console.log(this.props);
+    return <AttendanceTable rehearsals={this.rehearsals()} performers={this.props.performers} records={this.props.records} />;
+  }
+});
+
+var AttendanceTable = React.createClass({
+  'records': function(performer) {
+    return this.props.records[performer.id];
+  },
+
+  'render': function() {
+    var headers = this.props.rehearsals.map(function(rehearsal) {
+      var start_date = new Date(rehearsal.start_date);
+      var date_str = (start_date.getMonth() + 1) + '-' + start_date.getDate();
+      return (
+        <th key={rehearsal.id}>{date_str}</th>
+      );
+    });
+
+    var rows = this.props.performers.map(function(performer) {
+      return (
+        <AttendanceTableRow performer={performer} rehearsals={this.props.rehearsals} records={this.records(performer)} key={performer.id}/>
+      );
+    }.bind(this));
+    return (
+      <table className="attendance">
+        <tbody>
+          <tr>
+            <th>#</th>
+            <th>Performer</th>
+            {headers}
+            <th>Missed Mondays</th>
+            <th>Sectionals Attended</th>
+            <th>Total Missed</th>
+          </tr>
+          {rows}
+        </tbody>
+      </table>
+    );
+  }
+});
+
+var AttendanceTableRow = React.createClass({
+  'performer': function() {
+    return this.props.performer;
+  },
+  'rehearsals': function() {
+    return this.props.rehearsals;
+  },
+  'records': function() {
+    return this.props.records;
+  },
+  'render': function() {
+    var missed = 0;
+    var sectionals = 0;
+    var records = this.rehearsals().map(function(rehearsal) {
+      var record = this.records()[rehearsal.id];
+      var display_class = 'unknown';
+      var symbol = '?';
+      if (record) {
+        if (record.present) {
+          display_class = 'present';
+          symbol = "\u2713";
+          if (rehearsal.attendance == 'optional') {
+            sectionals += rehearsal.weight;
+          }
+        } else {
+          display_class = 'absent';
+          symbol = "\u2717";
+          if (rehearsal.attendance == 'required') {
+            missed += rehearsal.weight;
+          }
+        }
+      }
+      symbol = symbol.repeat(rehearsal.weight);
+      console.log(rehearsal.display_name);
+      var start_date = new Date(rehearsal.start_date);
+      var title = (start_date.getMonth() + 1) + '-' + start_date.getDate();
+      if (rehearsal.name) {
+        title = rehearsal.name + ' (' + title + ')';
+      }
+      return (
+        <td key={rehearsal.id}
+            className={display_class + ' record'}
+            title={title}>
+          {symbol}
+        </td>
+      );
+    }.bind(this));
+    return (
+      <tr>
+        <td>{this.performer().chorus_number}</td>
+        <td>{this.performer().name}</td>
+        {records}
+        <td>{missed}</td>
+        <td>{sectionals}</td>
+        <td>{missed - sectionals}</td>
+      </tr>
+    );
+  }
+});
--- app/controllers/attendance_controller.rb
+++ app/controllers/attendance_controller.rb
@@ -5,15 +5,17 @@ class AttendanceController < ApplicationController
   end
 
   def list
-    @registrations = Registration.current.order(:chorus_number)
-    @rehearsals = Concert.current.rehearsals.order(:start_date).index_by(&:id)
     @performers = []
     @records = {}
-    @registrations.each do |registration|
-      @performers << registration.performer
-      @records[registration.performer.id] = {}
+    Registration.current.order(:chorus_number).each do |registration|
+      performer = registration.performer.attributes
+      @records[performer['id']] = {}
+      performer['chorus_number'] = registration.chorus_number
+      @performers << performer
     end
-    AttendanceRecord.where(:performer => @performers, :rehearsal => @rehearsals.values).each do |record|
+    @rehearsals = Concert.current.rehearsals.order(:start_date)
+
+    AttendanceRecord.where(:performer => @performers.map{|performer| performer['id']}, :rehearsal => @rehearsals).each do |record|
       @records[record.performer.id][record.rehearsal.id] = record
     end
   end
