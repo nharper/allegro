e8f6b9e2b8093ca7494360b12a390af011eec595
Nick Harper <nharper@nharper.org>
2018-02-03 05:59:15 +0000

    Fix photo rendering

--- app/controllers/performers_controller.rb
+++ app/controllers/performers_controller.rb
@@ -27,13 +27,13 @@ class PerformersController < ApplicationController
   def photo
     @performer = Performer.find(params[:id])
     if @performer.photo
-      render :text => @performer.photo, :content_type => 'image/png'
+      render :plain => @performer.photo, :content_type => 'image/png'
     else
       # TODO(nharper): consider changing to clear.gif
       # http://upload.wikimedia.org/wikipedia/en/d/d0/Clear.gif
       #
       # 1x1 transparent png; source: http://garethrees.org/2007/11/14/pngcrush/
-      render :text => Base64.decode64('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=='), :content_type => 'image/png'
+      render :plain => Base64.decode64('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=='), :content_type => 'image/png'
     end
   end
 
