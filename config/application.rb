f546140a7db4bfc469962bb1979d2634dd20ddc9
Nick Harper <nharper@nharper.org>
2023-09-06 05:25:38 +0000

    Set config.active_record.belongs_to_required_by_default = false

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Allegro
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
 
+    config.active_record.belongs_to_required_by_default = false
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
