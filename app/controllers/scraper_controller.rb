74e38946cd55501d219e8eb94fb1b190260d4b28
Nick Harper <github@nharper.org>
2026-04-13 18:28:35 +0000

    Bump release-version in CCScraper

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -213,7 +213,7 @@ class CCScraper
     @http_conn = Net::HTTP.new('api.chorusconnection.com', 443)
     @http_conn.use_ssl = true
 
-    @headers = {'Release-Version': '12.0.6'}
+    @headers = {'Release-Version': '12.1.0'}
     @cookies = []
     if cookies
       @cookies = cookies
